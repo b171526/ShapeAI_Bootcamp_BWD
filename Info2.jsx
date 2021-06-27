4749a30505edaf387fa92c01302eadc5bc829ad3
b171526 <86550793+b171526@users.noreply.github.com>
2021-06-27 14:19:26 +0530

    Add files via upload

---
+++ Info2.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info2(){
+  return(
+    <div className="note">
+      <p>7 Days,Certified, Free Bootcamp in collaboration with Google Developer Groups, Microsoft Learn Student Ambassodor Program, and AWS Students Club.</p>
+      </div>
+  
+  );
+}
+export default Info2;
